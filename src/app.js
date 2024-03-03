8002c4c0e698eeba99a7fa7b9c9fe25f46f313d7
Fvelizb <155122487+Fvelizb@users.noreply.github.com>
2024-03-03 19:17:04 +0000

    app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["la", "mi", "nuestra", "tu"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let adj = ["gran", "linda", "fea"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let noun = ["casa", "perra", "gata", "ciudad"];
+
+let domain = [".com", ".cl", ".tv", ".gov"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < domain.length; l++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}`);
+      }
+    }
+  }
+}
